01934be9c56c7e2aad175d0d02e397d4f6e8063d
Adam Kreitzman <akreitzm@ucsd.edu>
2023-06-07 11:03:49 -0700

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "Hyper Dogechain"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "Hyper Doge"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "HDOGE"
 )
 
 var ID ids.ID
